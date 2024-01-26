c3c697da36654e99603914ace6e52b9781eee59b
Aqli Hamdan <42692072+aqlihammdan@users.noreply.github.com>
2024-01-27 02:39:04 +0700

    Update 2023_06_24_070414_create_laporans_table.php

--- database/migrations/2023_06_24_070414_create_laporans_table.php
+++ database/migrations/2023_06_24_070414_create_laporans_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('bulanbayar');
             $table->string('file_bukti');
             $table->timestamps();
+            $table->string('status')->nullable();
         });
     }
     
