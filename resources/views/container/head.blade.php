f1b3ab6442b6e730e1eab5f75835c38993a2524c
Aqli Hamdan <aqli.hammdan46@gmail.com>
2024-01-27 00:59:07 +0700

    Update head.blade.php

--- resources/views/container/head.blade.php
+++ resources/views/container/head.blade.php
@@ -8,3 +8,5 @@
   <link rel="stylesheet" href="{{ asset('AdminLTE/plugins/fontawesome-free/css/all.min.css') }}">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{ asset('AdminLTE/dist/css/adminlte.min.css') }}">
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
